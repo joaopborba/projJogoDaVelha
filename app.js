80b17b89bb3d1f5fc6c0c2074cb52491294da301
joaopborba <joaopborba95@hotmail.com>
2024-11-01 11:29:39 -0300

    atualização

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const player1 = document.getElementById('player1');
-const player2 = document.getElementById('player2');
-const play = document.getElementById('play');
-const playerTurn = document.getElementById('playerTurn');
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+const play = document.getElementById("play");
+const board = document.getElementById("board");
+let playerTurn = "";
 
+function updatePlayer() {
+  const playerInput = document.getElementById(playerTurn);
+  document.getElementById("playerTurn").innerText = playerInput.value;
+}
 
-play.addEventListener('click', function(){
-    playerTurn.innerText = player1.value;
-})
-
-
+play.addEventListener("click", function () {
+  playerInput.innerText = player1.value;
+});
